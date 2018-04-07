341976ee9c679ed0e1a24b7fb955c4ff13051a6c
Gautam Kumar <guruonly007@gmail.com>
2018-04-08 00:03:17 +0530

    add label

--- src/components/Matrix.vue
+++ src/components/Matrix.vue
@@ -6,48 +6,40 @@
                 <div class="card-header">Do First</div>
                 <div class="card-body">
                     <div class="h-100">
-                        <ul class="list-group list-group-flush p-0">
-                            <li class="list-group-item mb-1 bg-dark p-1" :bind="todo in todos">
-                                <div v-show="todo.edit == false">
-                                    <label class="w-100 m-0" @dblclick="todo.edit = true"> {{todo.title}} </label>
-                                </div>
-                                <input class="w-100 border-0 tsext-white" v-show="todo.edit == true" v-model="todo.title" v-on:blur="todo.edit=false; $emit('update')" @keyup.enter="todo.edit=false; $emit('update')">
+                         <ul class="list-group list-group-flush p-0">
+                            <li class="list-group-item mb-1 bg-dark p-1" v-for="tasks in tasks">
+                               <label class="mb-0" v-if="editTask = editTask" > {{tasks.title}}</label>
+                               <input type="text" v-model="task" @dblclick="editTask">
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-sm-12 col-md-6">
+        <!-- <div class="col-sm-12 col-md-6">
             <div class="card text-white bg-primary mb-3">
                 <div class="card-header">Schedule</div>
                 <div class="card-body">
                     <div class="h-100">
-                        <ul class="list-group list-group-flush p-0">
-                            <li class="list-group-item mb-1 bg-dark p-1" :bind="todo in todos">
-                                <div v-show="todo.edit == false">
-                                    <label class="w-100 m-0" @dblclick="todo.edit = true"> {{todo.title}} </label>
-                                </div>
-                                <input class="w-100 border-0 tsext-white" v-show="todo.edit == true" v-model="todo.title" v-on:blur="todo.edit=false; $emit('update')" @keyup.enter="todo.edit=false; $emit('update')">
+                         <ul class="list-group list-group-flush p-0">
+                            <li class="list-group-item mb-1 bg-dark p-1" :bind="task in tasks">
+                                <input type="text" class="w-100 border-0">
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-sm-12 col-md-6">
             <div class="card text-white bg-warning mb-3">
                 <div class="card-header">Delegate</div>
                 <div class="card-body">
                     <div class="h-100">
-                        <ul class="list-group list-group-flush p-0">
-                            <li class="list-group-item mb-1 bg-dark p-1" :bind="todo in todos">
-                                <div v-show="todo.edit == false">
-                                    <label class="w-100 m-0" @dblclick="todo.edit = true"> {{todo.title}} </label>
-                                </div>
-                                <input class="w-100 border-0 tsext-white" v-show="todo.edit == true" v-model="todo.title" v-on:blur="todo.edit=false; $emit('update')" @keyup.enter="todo.edit=false; $emit('update')">
+                         <ul class="list-group list-group-flush p-0">
+                            <li class="list-group-item mb-1 bg-dark p-1" :bind="task in tasks">
+                                <input type="text" class="w-100 border-0">
                             </li>
                         </ul>
                     </div>
@@ -59,19 +51,16 @@
                 <div class="card-header">Trivial</div>
                 <div class="card-body">
                     <div class="h-100">
-                        <ul class="list-group list-group-flush p-0">
-                            <li class="list-group-item mb-1 bg-dark p-1" :bind="todo in todos">
-                                <div v-show="todo.edit == false">
-                                    <label class="w-100 m-0" @dblclick="todo.edit = true"> {{todo.title}} </label>
-                                </div>
-                                <input class="w-100 border-0 tsext-white" v-show="todo.edit == true" v-model="todo.title" v-on:blur="todo.edit=false; $emit('update')" @keyup.enter="todo.edit=false; $emit('update')">
+                         <ul class="list-group list-group-flush p-0">
+                            <li class="list-group-item mb-1 bg-dark p-1" :bind="task in tasks">
+                                <input type="text" class="w-100 border-0">
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
   </div>
 
 </template>
@@ -81,16 +70,17 @@ export default {
   name: 'Matrix',
   data(){
   return {
-    todos:[{'title':'one value','edit':false},
-          {'title':'one value','edit':false},
-          {'title':'otro titulo','edit':false}],
-    }
+    tasks:[{'title':'one value','edit':false}]
+    },
+
   },
   methods: {
-        todo: function (edit) {
-        // `this` inside methods point to the Vue instance
-        alert(this.edit)
-    }
+    editTask: function (edit) {
+        alert('editTask');
+    },
+    // edit:function () {
+    //     alert(0)
+    // }
   },
   watch:{
       todo: function(todo){
