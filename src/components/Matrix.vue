4fd36e97f5fd5c93b469fdc724ee7fb930642cb6
Gautam Kumar <guruonly007@gmail.com>
2018-04-09 17:31:46 +0530

    Create Task componenet and add edit tasks fetaure

--- src/components/Matrix.vue
+++ src/components/Matrix.vue
@@ -2,110 +2,32 @@
   <div class="container my-5">
     <div class="row">
         <div class="col-sm-12 col-md-6">
-            <div class="card text-white bg-success mb-3">
-                <div class="card-header">Do First</div>
-                <div class="card-body">
-                    <div class="h-100">
-                         <ul class="list-group list-group-flush p-0">
-                            <li class="list-group-item mb-1 bg-dark p-1" v-for="task in tasks">
-                               <div v-if="task.edit == false">
-                                <label @dblclick="task.edit = true" class="mb-0">
-                                    {{ task.title }}
-                                </label>
-                               </div>
-                                <input @keyup.enter="addTask" type="text" v-model="newTask" v-if="task.edit == true" placeholder="edit" value="">
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <Tasks taskTitle='Do first' backgroundClass='bg-success'/>
         </div>
-        <!-- <div class="col-sm-12 col-md-6">
-            <div class="card text-white bg-primary mb-3">
-                <div class="card-header">Schedule</div>
-                <div class="card-body">
-                    <div class="h-100">
-                         <ul class="list-group list-group-flush p-0">
-                            <li class="list-group-item mb-1 bg-dark p-1" :bind="task in tasks">
-                                <input type="text" class="w-100 border-0">
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div> -->
+        <div class="col-sm-12 col-md-6">
+            <Tasks taskTitle='Schedule' backgroundClass='bg-primary'/>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <Tasks taskTitle='Delegate' backgroundClass='bg-warning'/>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <Tasks taskTitle='Trivial' backgroundClass='bg-danger'/>
+        </div>
     </div>
-    <!-- <div class="row">
-        <div class="col-sm-12 col-md-6">
-            <div class="card text-white bg-warning mb-3">
-                <div class="card-header">Delegate</div>
-                <div class="card-body">
-                    <div class="h-100">
-                         <ul class="list-group list-group-flush p-0">
-                            <li class="list-group-item mb-1 bg-dark p-1" :bind="task in tasks">
-                                <input type="text" class="w-100 border-0">
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-12 col-md-6">
-            <div class="card text-white bg-danger mb-3">
-                <div class="card-header">Trivial</div>
-                <div class="card-body">
-                    <div class="h-100">
-                         <ul class="list-group list-group-flush p-0">
-                            <li class="list-group-item mb-1 bg-dark p-1" :bind="task in tasks">
-                                <input type="text" class="w-100 border-0">
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
   </div>
-
 </template>
 
 <script>
+import Tasks from './Tasks.vue'
 export default {
-  name: 'Matrix',
-  props: ['todos'],
-   data(){
-       return{
-           tasks: [{'title': 'some task', 'edit': false}],
-           newTask: '',
-           editTask: null
-       }
-   },
-  methods: {
-    addTask: function(){
-        this.tasks.push({
-            taskItem: this.newTask
-        })
-        this.newTask = '';
-        // completed: false,
-    },
-    editTask: function(task){
-        this.editTask = task;
-        // console.log(task);
+    name: 'Matrix',
+    components: {
+        Tasks
     }
-  },
-  watch:{
-      todo: function(todo){
-        console.log(this.todo);
-      }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-input{
-    width: 100%;
-    background: #3dca54;
-    border: 0;
-}
+    /**/
 </style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="card text-white mb-3" :class='backgroundClass'>
+    <div class="card-header">{{ taskTitle }}</div>
+    <div class="card-body">
+        <div class="h-100">
+                <ul class="list-group list-group-flush p-0">
+                <li class="list-group-item mb-1 bg-transparent p-1">
+                    <input class="rounded-0 form-control form-control-sm" @keyup.enter="addTask" type="text" v-model="newTask" placeholder="Add Task">
+                </li>
+                <li class="list-group-item mb-1 bg-transparent p-1" v-for="task in tasks" :key='task.taksItem'>
+                    <div v-if="task.edit == false">
+                    <label @dblclick="task.edit = true" class="mb-0 p-1">
+                        {{ task.taskItem }}
+                    </label>
+                    </div>
+                    <input class="rounded-0 form-control form-control-sm" v-else type="text" v-model="task.taskItem" placeholder="Edit Task" @focusout="task.edit = false">
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Tasks',
+    props: {
+        'taskTitle': String,
+        'backgroundClass': String
+    },
+    data(){
+       return{
+           tasks: [],
+           newTask: ''
+       }
+   },
+
+  methods: {
+    created(){
+        tasks.length < 1
+        console.log(this.tasks.length);
+    },
+    addTask: function(){
+        this.tasks.push({
+            taskItem: this.newTask,
+            edit: false
+        })
+        this.newTask = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+input,
+.list-group-item > div{
+    background: #ffffff4f;
+    border: 0;
+}
+</style>
+
+
