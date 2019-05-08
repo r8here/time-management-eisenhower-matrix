408cc9546f4306bcee3ce0725cc15d2c7846a274
Gautam Kumar <guruonly007@gmail.com>
2019-05-08 11:33:02 +0530

    Remove jQuery & Popperjs

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery'
-import 'popper.js'
-import 'bootstrap'
+
 
 new Vue({
   render: h => h(App)
